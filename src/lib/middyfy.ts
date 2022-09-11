0a44558158c3b29cb8807d3acb14340b1e9dfb7b
Kauê Leal de Lima <86488715+kauelima21@users.noreply.github.com>
2022-09-11 12:53:17 -0300

    Create middyfy.ts

---
+++ src/lib/middyfy.ts
@@ -0,0 +1,11 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import jsonBodyParser from "@middy/http-json-body-parser";
+
+export default handler => middy(handler)
+  .use([
+    jsonBodyParser(),
+    httpEventNormalizer(),
+    httpErrorHandler(),
+  ]);
