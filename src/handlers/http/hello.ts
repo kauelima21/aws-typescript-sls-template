f3e05b23e8e8b2c76c3d4c1364c7a561ecd9281a
kauelima21 <kaueslim@outlook.com>
2022-09-23 14:42:32 -0300

    update folder structure

--- src/handlers/http/hello.ts
+++ src/handlers/http/hello.ts
@@ -1,5 +1,5 @@
+import { formatJSONResponse } from "@lib/json-response";
 import { APIGatewayEvent } from "aws-lambda";
-import { formatJSONResponse } from "../lib/json-response";
 
 async function hello(event: APIGatewayEvent) {
   console.log(event);
